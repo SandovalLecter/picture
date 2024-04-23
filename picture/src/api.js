d2736979ff2a875a6b2b74c68ec1c4515b3e985d
SandovalLecter <sandovalrojaseligio@cbtis116.edu.mx>
2024-04-23 14:51:52 -0700

    Inicial commit

--- picture/src/api.js
+++ picture/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const searchImage = async(term) => {
-    const url = 'https://api.unsplash.com/search/photos/?client_id=6tOYXzh2nzaWSS2_6BL0Jmx-rBQDTWN2KrP2UugZYew'
+    const url = 'https://api.unsplash.com/search/photos/?client_id=-XBP9sD8ouHffRZK5n908NSxd6ne_96DjA3CcJyMFE0'
     const response = await axios.get(url, {
         params: {
             query: term
